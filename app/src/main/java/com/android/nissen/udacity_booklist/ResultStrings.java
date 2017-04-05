363f901bf9f13ce1f38d401991d3c621669a1a67
josh-nissen <josh.e.nissen@outlook.com>
2017-04-05 08:34:39 -0400

    Intial Commit

---
+++ app/src/main/java/com/android/nissen/udacity_booklist/ResultStrings.java
@@ -0,0 +1,44 @@
+package com.android.nissen.udacity_booklist;
+
+/**
+ * Created by Josh Nissen on 4/5/2017.
+ */
+
+public class ResultStrings {
+
+    private String mTitle = "";
+    private String mAuthor = "";
+    private String mPublisher = "";
+
+    public ResultStrings(String title, String author, String publisher) {
+        mTitle = title;
+        mAuthor = author;
+        mPublisher = publisher;
+    }
+    // Getter Methods
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public String getPublisher() {
+        return mPublisher;
+    }
+
+    // Setter methods
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setPublisher(String publisher) {
+        mPublisher = publisher;
+    }
+
+    public void setAuthor(String author) {
+        mAuthor = author;
+    }
+
+}
